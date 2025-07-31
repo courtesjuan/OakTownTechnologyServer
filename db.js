8dea3849f716129b0cc0a7f9cefd26742d69796a
courtesjuan <114782250+courtesjuan@users.noreply.github.com>
2025-07-31 00:22:51 -0700

    Update db.js to suparbase

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
-const mysql = require('mysql2/promise');
+// db.js
+require('dotenv').config();        // load DATABASE_URL from .env
+const { Pool } = require('pg');
 
-const pool = mysql.createPool({
-  host: 'oaktowntechnology.c76gg4wwio3z.us-east-1.rds.amazonaws.com', // RDS endpoint
-  user: 'admin', // Your RDS master username
-  password: 'oaktowntechnology',    // Your RDS password
-  database: 'oaktowntech', // The database name (create one if not already)
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }   // required by Supabase
 });
 
 module.exports = pool;
-
